2409955ad9e441b6b9b6f8aec52f32bcbd7d38a3
Pan Deran <panderan@163.com>
2019-05-23 11:36:42 +0800

    Eliminate redundancy

--- main/main.js
+++ main/main.js
@@ -1,11 +1,11 @@
+function nonnegative(num) {
+    return num>0 ? num:0 
+}
+
 module.exports = function main(inputs) {
-    price = 6;
-    if (8 - inputs.distance < 6) {
-        price += (inputs.distance-2)*0.8
-    }
-    if (8 - inputs.distance < 0) {
-        price += (inputs.distance-8)*0.4
-    }
+    price = (nonnegative(inputs.distance - 8)/2 + 
+             nonnegative(inputs.distance -2)) *
+            0.8
     price += inputs.parkTime*0.25
-    return Math.floor(price+0.5)
+    return Math.floor(price+6.5)
 };
