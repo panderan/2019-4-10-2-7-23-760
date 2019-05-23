fe8f3ffe1801c92a8c44e7f3fb78623652835b87
Pan Deran <panderan@163.com>
2019-05-23 11:14:06 +0800

    make 'more than 8km and parking' green

--- main/main.js
+++ main/main.js
@@ -1,7 +1,10 @@
 module.exports = function main(inputs) {
     price = 6;
-    if (10 - inputs.distance < 8) {
+    if (8 - inputs.distance < 6) {
         price += (inputs.distance-2)*0.8
+    }
+    if (8 - inputs.distance < 0) {
+        price += (inputs.distance-8)*0.4
     }
     price += inputs.parkTime*0.25
     return Math.floor(price+0.5)
